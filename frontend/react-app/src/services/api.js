816d4082c3488bde3e397811faba5cd7f0e91909
Shraddha571997 <shraddhagudasalamani.mca2024@adhiyamaan.in>
2026-04-16 01:19:29 +0530

    Refactor API base URL to use environment variable
    Updated API base URL to use environment variable or fallback to default.

--- frontend/react-app/src/services/api.js
+++ frontend/react-app/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 // 🔥 FIXED: Direct EC2 backend URL
-const API_BASE_URL = "http://16.170.201.149:5000/api";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "/api"
 
 const api = axios.create({
   baseURL: API_BASE_URL,
